f431e70205f368a99d56fcb17e06460d14ba05e2
Frederick Wong <frederick.kf.wong@gmail.com>
2014-11-28 12:45:31 +1100

    Created validations for artist

--- w03/d05/frederick/moma/db/seeds.rb
+++ w03/d05/frederick/moma/db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+davida = Artist.create({
+	first_name: "Davida",
+	last_name: "Allen",
+	period: "PO-POMO",
+	nationality: "Australian",
+	date_of_birth: "20/10/1953",
+	image: "http://www.madaboutdogs.com.au/hinchinbrook/art/images/Elizabeth%20Cummings%20-%20Summer%20Dugongs.JPG"
+	})
+
+davida.paintings.create({
+	title: "Lorem Ipsum",
+	year: "1970",
+	medium: "Oil on board",
+	style: "Cubist",
+	image: "http://www.madaboutdogs.com.au/hinchinbrook/art/images/Elizabeth%20Cummings%20-%20Summer%20Dugongs.JPG"
+	
+	})
+
+davida.paintings.create({
+	title: "Dolar sit",
+	year: "1970",
+	medium: "Oil on board",
+	style: "Cubist",
+	image: "http://www.madaboutdogs.com.au/hinchinbrook/art/images/Elizabeth%20Cummings%20-%20Summer%20Dugongs.JPG"
+	
+	})
+
+davida.paintings.create({
+	title: "Amet",
+	year: "1970",
+	medium: "Oil on board",
+	style: "Cubist",
+	image: "http://www.madaboutdogs.com.au/hinchinbrook/art/images/Elizabeth%20Cummings%20-%20Summer%20Dugongs.JPG"
+	
+	})
